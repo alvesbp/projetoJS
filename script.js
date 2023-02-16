05e020c6904bf611421d00d4408889a762d81495
Matheus Brito ££ <matheus.alvesdb@gmail.com>
2023-02-16 15:15:09 -0300

    add scroll suave

--- script.js
+++ script.js
@@ -37,3 +37,27 @@ function initAccordion() {
   }
 }
 initAccordion();
+
+const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+
+function initScrollSuave() {
+  function scrollToSection(event) {
+    event.preventDefault();
+    const href = this.getAttribute("href");
+    const section = document.querySelector(href);
+
+    section.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+    // forma alternativa
+    //   window.scrollTo({
+    //     top: section.offsetTop,
+    //     behavior: "smooth",
+    //   });
+  }
+  linksInternos.forEach((link) => {
+    link.addEventListener("click", scrollToSection);
+  });
+}
+initScrollSuave();
