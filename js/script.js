780123bbca385a5454df3252f856bac5e370fb36
Matheus Brito ££ <matheus.alvesdb@gmail.com>
2023-03-14 10:49:48 -0300

    adicionado tooltip no mapa

--- js/script.js
+++ js/script.js
@@ -3,9 +3,11 @@ import initAnimacaoScroll from "./modules/scroll-animacao.js";
 import initAccordion from "./modules/accordion.js";
 import initNavTab from "./modules/tabnav.js";
 import initModal from "./modules/modal.js";
+import initTooltip from "./modules/tooltip.js";
 
 initScrollSuave();
 initAnimacaoScroll();
 initAccordion();
 initNavTab();
 initModal();
+initTooltip();
