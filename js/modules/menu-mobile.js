19d9c652732a12cf63ff9aa697410eae1422cb76
Matheus Brito ££ <matheus.alvesdb@gmail.com>
2023-03-22 08:02:31 -0300

    adicionado menu-mobile

---
+++ js/modules/menu-mobile.js
@@ -0,0 +1,19 @@
+import outsideClick from "./outsideClick.js";
+export default function initMenuMobile() {
+  const menuButton = document.querySelector('[data-menu="button"]');
+  const menuList = document.querySelector('[data-menu="list"]');
+  const events = ["click", "touchstart"];
+
+  if (menuButton) {
+    function openMenu(event) {
+      menuList.classList.add("ativo");
+      menuButton.classList.add("ativo");
+      outsideClick(menuList, events, () => {
+        menuList.classList.remove("ativo");
+        menuButton.classList.remove("ativo");
+      });
+    }
+
+    events.forEach((event) => menuButton.addEventListener(event, openMenu));
+  }
+}
--- js/script.js
+++ js/script.js
@@ -4,10 +4,13 @@ import initAccordion from "./modules/accordion.js";
 import initNavTab from "./modules/tabnav.js";
 import initModal from "./modules/modal.js";
 import initTooltip from "./modules/tooltip.js";
-
+import initDropdownMenu from "./modules/dropdown-menu.js";
+import initMenuMobile from "./modules/menu-mobile.js";
 initScrollSuave();
 initAnimacaoScroll();
 initAccordion();
 initNavTab();
 initModal();
 initTooltip();
+initDropdownMenu();
+initMenuMobile();
